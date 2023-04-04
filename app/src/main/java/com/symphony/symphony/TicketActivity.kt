8f5cfc38b7dd5158e364b1e171308786871ca3cb
kygo <leekygo3@gmail.com>
2023-04-04 15:33:09 +0300

    Integrated firebase storage for jobcards

--- app/src/main/java/com/symphony/symphony/TicketActivity.kt
+++ app/src/main/java/com/symphony/symphony/TicketActivity.kt
@@ -1,39 +1,34 @@
 package com.symphony.symphony
 
 import android.app.Activity
+import android.content.ContentResolver
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
-import android.util.Base64
 import android.util.Log
 import android.view.View
+import android.webkit.MimeTypeMap
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.android.volley.NetworkError
 import com.android.volley.NetworkResponse
-import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.HttpHeaderParser
-import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import com.google.gson.Gson
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.OnProgressListener
+import com.google.firebase.storage.StorageReference
 import com.symphony.symphony.databinding.ActivityTicketBinding
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.MultipartBody
-import okhttp3.OkHttpClient
-import okhttp3.RequestBody
-import org.json.JSONException
 import org.json.JSONObject
-import java.io.ByteArrayOutputStream
-import java.io.File
-import java.nio.charset.Charset
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -55,13 +50,15 @@ class TicketActivity : AppCompatActivity() {
     private lateinit var updateT: Button
     private lateinit var takePic: ImageView
     private lateinit var byteArray: ByteArray
+    private lateinit var attachment: String
     private lateinit var imageBitmap: Bitmap
     private var imageData: ByteArray? = null
     private val REQUEST_IMAGE_CAPTURE = 1
     private val PICK_PDF_FILE = 23
     val REQUEST_CODE = 1
-    private lateinit var filePath : File
-
+    private lateinit var filePath: Uri
+    private val root: DatabaseReference = FirebaseDatabase.getInstance().getReference("Jobcards")
+    private val reference: StorageReference = FirebaseStorage.getInstance().getReference()
 
 
     companion object {
@@ -107,29 +104,8 @@ class TicketActivity : AppCompatActivity() {
         val takePic = binding.imgCamera
         takePic.setOnClickListener {
 
-            val permission = android.Manifest.permission.READ_EXTERNAL_STORAGE
-            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, arrayOf(permission), REQUEST_CODE)
-            } else {
-              openPdfPicker()
-            }
+            openGallery()
 
-
-
-//            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
-//                == PackageManager.PERMISSION_GRANTED
-//            ) {
-//                // Permission is granted
-//
-//                val picIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//                startActivityForResult(picIntent, REQUEST_IMAGE_CAPTURE)
-//            } else {
-//                // Permission is not granted, request it
-//                ActivityCompat.requestPermissions(
-//                    this, arrayOf(android.Manifest.permission.CAMERA),
-//                    REQUEST_IMAGE_CAPTURE
-//                )
-//            }
         }
 
         updateT.setOnClickListener {
@@ -143,8 +119,14 @@ class TicketActivity : AppCompatActivity() {
             val action_taken = binding.edtTDActionsTakenValue.text.toString()
             val recommendations = binding.edtTDRecommendationsValue.text.toString()
 
+            if (filePath != null) {
+                uploadToFirebase(filePath)
+            } else {
+                Toast.makeText(this, "No image selected", Toast.LENGTH_SHORT).show()
+            }
 
-            uploadPdf(filePath)
+
+//            uploadPdf(filePath)
 //            uploadImage(imageBitmap)
 //            uploadOk(imageBitmap)
 //            uploadVolley()
@@ -197,40 +179,41 @@ class TicketActivity : AppCompatActivity() {
 
     }
 
-    private fun uploadPdf(file: File) {
-        val url = "https://backend.api.symphony.co.ke/uploadPdf"
+    private fun uploadToFirebase(uri: Uri) {
+        val fileRef = reference.child("${System.currentTimeMillis()}" + "." + getFileExtension(uri))
+        fileRef.putFile(uri).addOnSuccessListener(OnSuccessListener { taskSnapshot ->
+            taskSnapshot.storage.downloadUrl.addOnSuccessListener { downloadUrl ->
+                // Use downloadUrl as needed
+                attachment = downloadUrl.toString()
+                Log.d("Attachment", "Image download URL: $attachment")
+                progressB.visibility = View.GONE
+                Toast.makeText(this, "Uploaded successfully", Toast.LENGTH_SHORT).show()
 
-        val fileName = file.name
-        val fileContent = file.readBytes()
-        val fileBase64 = Base64.encodeToString(fileContent, Base64.DEFAULT)
+            }.addOnFailureListener { exception ->
+                // Handle any errors
+                Log.e("Attachment", "Error getting download URL: ${exception.message}", exception)
+            }
 
-        val requestBody = mapOf(
-            "file" to fileBase64,
-            "fileName" to fileName
-        )
 
-        val request = okhttp3.Request.Builder()
-            .url(url)
-            .post(
-                RequestBody.create(
-                    "application/json".toMediaTypeOrNull(),
-                    Gson().toJson(requestBody)
-                )
-            )
-            .build()
+        })
+            .addOnProgressListener(OnProgressListener {
+                progressB.visibility = View.VISIBLE
+            }).addOnFailureListener(
+                OnFailureListener { exception ->
+                    Log.e("Firebase", "Upload failed", exception)
 
-        val response = OkHttpClient().newCall(request).execute()
+                    progressB.visibility = View.GONE
 
-        if (response.isSuccessful) {
-            // File uploaded successfully
-            Toast.makeText(this, response.toString(), Toast.LENGTH_SHORT).show()
-
-        } else {
-            // Error occurred while uploading file
-            Toast.makeText(this, response.toString(), Toast.LENGTH_SHORT).show()
-
-        }
+                    Toast.makeText(this, "JobCard upload failed", Toast.LENGTH_SHORT).show()
+                })
     }
+
+    private fun getFileExtension(mUri: Uri): String? {
+        val contentResolver: ContentResolver = getContentResolver()
+        val mime: MimeTypeMap = MimeTypeMap.getSingleton()
+        return mime.getExtensionFromMimeType(contentResolver.getType(mUri))
+    }
+
 
     private fun openPdfPicker() {
         val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
@@ -240,40 +223,39 @@ class TicketActivity : AppCompatActivity() {
         startActivityForResult(intent, PICK_PDF_FILE)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == PICK_PDF_FILE && resultCode == Activity.RESULT_OK) {
-            val fileUri = data?.data
-            if (fileUri != null) {
-                try {
-                    // Open a stream to read the content of the selected file
-                    val inputStream = contentResolver.openInputStream(fileUri)
-
-                    if (inputStream != null) {
-                        // The user has selected a valid file
-                        Toast.makeText(this, "Pdf selected", Toast.LENGTH_SHORT).show()
-
-                        // Get the file path from the file URI
-                         filePath = File(fileUri.path)
-
-
-                    } else {
-                        // Failed to open the stream for the selected file
-                        Toast.makeText(this, "Failed to read the selected file", Toast.LENGTH_SHORT).show()
-                    }
-                } catch (e: Exception) {
-                    // Failed to open the selected file
-                    Toast.makeText(this, "Failed to open the selected file", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                // The user did not select a valid file
-                Toast.makeText(this, "No Pdf selected", Toast.LENGTH_SHORT).show()
-                // Show an error message or handle the error appropriately
-            }
-        }
-    }
-
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        if (requestCode == PICK_PDF_FILE && resultCode == Activity.RESULT_OK) {
+//            val fileUri = data?.data
+//            if (fileUri != null) {
+//                try {
+//                    // Open a stream to read the content of the selected file
+//                    val inputStream = contentResolver.openInputStream(fileUri)
+//
+//                    if (inputStream != null) {
+//                        // The user has selected a valid file
+//                        Toast.makeText(this, "Pdf selected", Toast.LENGTH_SHORT).show()
+//
+//                        // Get the file path from the file URI
+//                         filePath = File(fileUri.path)
+//
+//
+//                    } else {
+//                        // Failed to open the stream for the selected file
+//                        Toast.makeText(this, "Failed to read the selected file", Toast.LENGTH_SHORT).show()
+//                    }
+//                } catch (e: Exception) {
+//                    // Failed to open the selected file
+//                    Toast.makeText(this, "Failed to open the selected file", Toast.LENGTH_SHORT).show()
+//                }
+//            } else {
+//                // The user did not select a valid file
+//                Toast.makeText(this, "No Pdf selected", Toast.LENGTH_SHORT).show()
+//                // Show an error message or handle the error appropriately
+//            }
+//        }
+//    }
 
 
 //    @Throws(IOException::class)
@@ -302,127 +284,82 @@ class TicketActivity : AppCompatActivity() {
 //    }
 
 
-
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
-//            val uri = data?.data
-//            if (uri != null) {
-//                binding.imgPreview.setImageURI(uri)
-//                createImageData(uri)
-//            }
-//        }
-//        super.onActivityResult(requestCode, resultCode, data)
-//    }
-
-    //    private fun openGallery() {
-//        val intent = Intent(Intent.ACTION_PICK)
-//        intent.type = "image/*"
-//        startActivityForResult(intent, IMAGE_PICK_CODE)
-//    }
-
-
-    private fun uploadOk(bitmap: Bitmap) {
-        val client = OkHttpClient.Builder().build()
-        val mediaType = "application/json".toMediaTypeOrNull()
-        val stream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
-        val byteArray = stream.toByteArray()
-        val file = File.createTempFile("image", ".jpeg")
-        file.writeBytes(byteArray)
-        val requestBody: RequestBody =
-            MultipartBody.Builder().setType(MultipartBody.FORM).addFormDataPart(
-                "image",
-                file.name,
-                RequestBody.create("application/octet-stream".toMediaTypeOrNull(), file)
-            ).build()
-
-        val request = okhttp3.Request.Builder()
-            .url("https://backend.api.symphony.co.ke/uploadVolley")
-            .method("POST", requestBody)
-            .addHeader("Content-Type", "application/json")
-            .build()
-
-        Thread {
-            try {
-                val response = client.newCall(request).execute()
-                println(response.body?.string())
-                runOnUiThread {
-                    Toast.makeText(this, response.body.toString(), Toast.LENGTH_SHORT).show()
-                }
-                Log.d("okhttp", response.body.toString())
-            } catch (e: Exception) {
-                runOnUiThread {
-                    Toast.makeText(this, e.message.toString(), Toast.LENGTH_SHORT).show()
-                }
-                Log.e("okhttp", "Error: ${e.message}")
-            }
-        }.start()
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
     }
 
-    private fun uploadVolley() {
-        imageData ?: return
-//        val postURL = "https://backend.api.symphony.co.ke/uploadI"
-        val postURL = "https://reqbin.com/"
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
-        val request =
-            object : VolleyFileUploadRequest(Request.Method.POST, postURL, Response.Listener {
-                Log.d("newvolley", "$it")
-                println("response is: $it")
-            }, Response.ErrorListener {
-                Log.d("newvolley", "$it")
-                println("error is: $it")
-            }) {
-                override fun getByteData(): MutableMap<String, FileDataPart> {
-                    var params = HashMap<String, FileDataPart>()
-                    params["imageFile"] = FileDataPart("image", imageData!!, "jpeg")
-                    return params
-                }
-            }
-        Volley.newRequestQueue(this).add(request)
-    }
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE && data != null) {
+            filePath = data.data!!
 
-
-    private fun uploadImage(bitmap: Bitmap) {
-        val byteArrayOutputStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream)
-        val image = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT)
-
-        Log.d("pic", image)
-        val name = "event_image"
-
-        try {
-            val jsonObject = JSONObject()
-            jsonObject.put("name", name)
-            jsonObject.put("image", image)
-
-            val jsonObjectRequest = JsonObjectRequest(Request.Method.POST,
-                "https://backend.api.symphony.co.ke/uploadI",
-                jsonObject,
-                { response ->
-                    try {
-                        val message = response.getString("message")
-                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-//                        progressDialog.dismiss()
-                    } catch (e: JSONException) {
-                        e.printStackTrace()
-                    }
-                },
-                { error ->
-                    Log.d("pic", error.toString())
-                    val errorMessage =
-                        error.networkResponse?.data?.toString(Charset.defaultCharset())
-                    Log.e("Volley Error", errorMessage ?: "Unknown error")
-                    Toast.makeText(this, error.toString(), Toast.LENGTH_SHORT).show()
-//                    progressDialog.dismiss()
-                })
-
-            val requestQueue = Volley.newRequestQueue(this)
-            requestQueue.add(jsonObjectRequest)
-
-        } catch (e: JSONException) {
-            e.printStackTrace()
         }
     }
+
+
+//private fun uploadOk(bitmap: Bitmap) {
+//    val client = OkHttpClient.Builder().build()
+//    val mediaType = "application/json".toMediaTypeOrNull()
+//    val stream = ByteArrayOutputStream()
+//    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+//    val byteArray = stream.toByteArray()
+//    val file = File.createTempFile("image", ".jpeg")
+//    file.writeBytes(byteArray)
+//    val requestBody: RequestBody =
+//        MultipartBody.Builder().setType(MultipartBody.FORM).addFormDataPart(
+//            "image",
+//            file.name,
+//            RequestBody.create("application/octet-stream".toMediaTypeOrNull(), file)
+//        ).build()
+//
+//    val request = okhttp3.Request.Builder()
+//        .url("https://backend.api.symphony.co.ke/uploadVolley")
+//        .method("POST", requestBody)
+//        .addHeader("Content-Type", "application/json")
+//        .build()
+//
+//    Thread {
+//        try {
+//            val response = client.newCall(request).execute()
+//            println(response.body?.string())
+//            runOnUiThread {
+//                Toast.makeText(this, response.body.toString(), Toast.LENGTH_SHORT).show()
+//            }
+//            Log.d("okhttp", response.body.toString())
+//        } catch (e: Exception) {
+//            runOnUiThread {
+//                Toast.makeText(this, e.message.toString(), Toast.LENGTH_SHORT).show()
+//            }
+//            Log.e("okhttp", "Error: ${e.message}")
+//        }
+//    }.start()
+//}
+
+//private fun uploadVolley() {
+//    imageData ?: return
+////        val postURL = "https://backend.api.symphony.co.ke/uploadI"
+//    val postURL = "https://reqbin.com/"
+//
+//    val request =
+//        object : VolleyFileUploadRequest(Request.Method.POST, postURL, Response.Listener {
+//            Log.d("newvolley", "$it")
+//            println("response is: $it")
+//        }, Response.ErrorListener {
+//            Log.d("newvolley", "$it")
+//            println("error is: $it")
+//        }) {
+//            override fun getByteData(): MutableMap<String, FileDataPart> {
+//                var params = HashMap<String, FileDataPart>()
+//                params["imageFile"] = FileDataPart("image", imageData!!, "jpeg")
+//                return params
+//            }
+//        }
+//    Volley.newRequestQueue(this).add(request)
+//}
+
 
     private fun sendTicketDetails(
         ticket_no: String,
