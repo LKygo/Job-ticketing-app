7b42e6f31d95814c83815e254ba778fc40f75dc3
Lee Kygo <leekygo3@gmail.com>
2023-09-10 22:14:00 +0300

    Update MainActivity.kt

--- app/src/main/java/com/symphony/symphony/MainActivity.kt
+++ app/src/main/java/com/symphony/symphony/MainActivity.kt
@@ -6,32 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    var url = "https://backend.api.symphony.co.ke/tickets"
+    var url = "Enter your url"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
     }
-
-//    private fun sendAndRequestResponse() {
-////       Initialize request queue
-//        val volley = Volley.newRequestQueue(this)
-//
-//        val stringRequest = StringRequest(
-//            Request.Method.GET, url,
-//            { response ->
-//
-//                for (i in 0..response.length){
-//
-//                    Log.d("API", response.toString())
-//
-//                }
-//            }
-//        ) { error -> Log.i("API", "Error :$error") }
-//
-//        volley.add(stringRequest)
-//
-//
-//    }
 }
